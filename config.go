7b26fda042db8781f5d2455bcb09c600f6b07763
charan906 <127377469+charan906@users.noreply.github.com>
2025-01-24 15:43:03 +0530

    Update config.go

--- config.go
+++ config.go
@@ -1,7 +1,7 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
-package transformprocessor // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/transformprocessor"
+package github.com/charan906/processor // import "github.com/open-telemetry/opentelemetry-collector-contrib/processor/transformprocessor"
 
 import (
 	"errors"
